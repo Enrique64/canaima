c41013b4be491edc5ed18045a5dacfd6eb77963f
root <root@canaima-popular.(none)>
2014-08-27 17:21:30 -0430

    subiendo mi primer archivo en git

---
+++ prueba.sh
@@ -0,0 +1,11 @@
+#!/bin basch
+echo script
+ 
+fecha=$(date +"%m-%d-%Y-%T")
+
+cp -R /etc/apt/ /home/sistema/control/apt$fecha
+echo $m
+NUMERO1=12,3
+NUMERO2=20,3
+RESULTADO=`echo "$NUMERO1 + $NUMERO2" | bc` 
+echo $RESULTADO
